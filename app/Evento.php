c91217fb0c304eecabf29beea664da87e016a9d7
kevin Natan <kevinnatan97@gmail.com>
2019-10-29 22:29:10 -0200

    feito os modelos e migration eventos

---
+++ app/Evento.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Evento extends Model
+{   protected $table = ['evento'];
+    protected $fillable = ['nome','data','descricao','local','hora'];
+
+    public function participante(){
+        return $this->belongsToMany('App\Participante', 'participantes_eventos', 'participante_id', 'evento_id');
+    }
+    
+}
