0ff4e5b2503b553ec325b36898cce1724021eb18
Joao Alberto <jooalberto1@gmail.com>
2019-10-30 21:27:13 -0300

    Adicionada lista de participantes

--- app/Evento.php
+++ app/Evento.php
@@ -9,7 +9,7 @@ class Evento extends Model
     protected $fillable = ['nome','data','descricao','local','hora'];
 
     public function participante(){
-        return $this->belongsToMany('App\Participante', 'participantes_eventos', 'participante_id', 'evento_id');
+        return $this->belongsToMany('App\Participante', 'participantes_eventos', 'evento_id', 'participante_id');
     }
     
 }
