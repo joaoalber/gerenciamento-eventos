6bb205ebe30d43fe5823143ccb086d8ade76c8af
kevin Natan <kevinnatan97@gmail.com>
2019-11-06 21:05:45 -0200

    frist commit

--- resources/views/adiciona.blade.php
+++ resources/views/adiciona.blade.php
@@ -13,34 +13,39 @@
 @endsection
 
 @section('content')
-<form method="POST" action="{{url('/evento/salva')}}">
-@csrf
+
+<div class="card" style="width: 40rem;">
+  <div class="card-header text-center">Adicionar Participante</div>
+  <div class="card-body">
+      <form method="POST" action="{{url('/evento/salva')}}">
+        @csrf
 
         <input type="hidden" name="evento_id" value="{{$data['evento_id']}}">
-        <div class="card mx-auto">
-        <div class="card-header text-center">
-          <h3>Selecionar Participantes</h3>
-        </div>
-        <div class="card-body text-left mx-auto">
-        <?php $i = 0; ?>
-        <!-- (Flávio) Checkbox agora vem com os participantes que pertencem a esse evento checkados. participantes[] agora tem um index diferente para cada opção (participantes[0], participantes[1], etc..). -->
-        @foreach($participantes as $participante)
-        <div class="custom-control custom-checkbox">
-               <input class="custom-control-input"  id="customCheck<?php echo $i ?>" type="checkbox" name="participantes[<?php echo $i ?>]" value="{{$participante->id}}" 
-               <?php 
-                    if(isset($evento_participante)){
-                      foreach ($evento_participante as $key) {
-                        if($key->pivot->participante_id == $participante->id){
-                          echo 'disabled';
-                        }
-                      }
-                    }
-                ?>>
-               <label class="custom-control-label" for="customCheck<?php echo $i ?>">{{$participante->nome}}</label>
-            </div>
-        <?php $i++; ?>
-        @endforeach
-        <input type="submit" value="Salvar" class="btn btn-primary mt-4">
-        </div>
-</form>
+
+              <?php $i = 0; ?>
+              <!-- (Flávio) Checkbox agora vem com os participantes que pertencem a esse evento checkados. participantes[] agora tem um index diferente para cada opção (participantes[0], participantes[1], etc..). -->
+              @foreach($participantes as $participante)
+              <div class="custom-control custom-checkbox">
+                    <input class="custom-control-input" id="customCheck<?php echo $i ?>" type="checkbox" name="participantes[<?php echo $i ?>]" value="{{$participante->id}}" 
+                    <?php 
+                          if(isset($evento_participante)){
+                            foreach ($evento_participante as $key) {
+                              if($key->pivot->participante_id == $participante->id){
+                                echo 'disabled';
+                              }
+                            }
+                          }
+                      ?>>
+                    <label class="custom-control-label" for="customCheck<?php echo $i ?>">{{$participante->nome}}</label>
+              </div>
+              <?php $i++; ?>
+              @endforeach
+
+      
+    </div>
+    <div class="card-footer d-flex justify-content-end"> 
+                <input type="submit" value="Salvar" class="btn btn-primary mt-4">
+              </div>
+    </form>
+    </div>
 @endsection
