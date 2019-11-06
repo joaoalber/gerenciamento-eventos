a80508421adaecc03006655220b59eb9616f1f90
kevin Natan <kevinnatan97@gmail.com>
2019-11-06 20:19:39 -0200

    Checkbox agora não é mais atualizado conforme adiciona os participantes, By: Spwn e Kevin

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\{Evento, Partcipante};
+use App\{Evento, Participante};
 use DB;
 
 class EventoController extends Controller
